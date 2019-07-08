30d37220b0a51df89fbe3b54b48622ad3bfb7162
Emilio Nyaray <emilio@nyaray.com>
2019-07-08 08:12:10 +0200

    Create test project for tft_rpi3

---
+++ lib/tfttest.ex
@@ -0,0 +1,18 @@
+defmodule Tfttest do
+  @moduledoc """
+  Documentation for Tfttest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tfttest.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tfttest_test.exs
@@ -0,0 +1,8 @@
+defmodule TfttestTest do
+  use ExUnit.Case
+  doctest Tfttest
+
+  test "greets the world" do
+    assert Tfttest.hello() == :world
+  end
+end
